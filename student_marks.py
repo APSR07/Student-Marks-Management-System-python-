f896441a8aa0762af668c72b39844e0e84f902f4
APSR07 <apsr725@gmail.com>
2026-07-11 21:45:08 +0530

    remove all unwanted functions

--- student_marks.py
+++ student_marks.py
@@ -3,7 +3,7 @@ studentMarks = []
 # Add student function:
 def addStudentAndMarks():
     # studentRange = int(input("enter how many student name you want to enter = "))
-    global studentNames, studentMarks # both list become global 
+     
     # for i in range (0, studentRange):
     #     print(f"{i+1} Enter Student Name : ")
     #     studentNames.append(input())
@@ -30,55 +30,16 @@ def addStudentAndMarks():
                     
 
             print(f"{i+1} Enter Student Marks : ")
-            Studnet_Marks = input()
+            Studnet_Marks = input().strip()
             while True:
                 if Studnet_Marks.isdigit():
-                    studentMarks.append(Studnet_Marks)
+                    studentMarks.append(int(Studnet_Marks))
                     break
                 else:
                     print("Invalid Input !. Try again")
                     return
         
             i += 1
-
-def display():
-    for i in range(len(studentNames)):
-        print(f"{studentNames[i]} : {studentMarks[i]}")
-    
-# Highest Marks
-def highest_Marks():
-    high = 0
-    max = int(studentMarks[0])
-    for i in range(len(studentMarks)):
-        cur_val = int(studentMarks[i])            # so if current is highest then it will stay high, if not then we update it
-        if cur_val > max:
-            max = cur_val
-            high = i
-    print(f"Hightest Marks is : {studentMarks[high]} by {studentNames[high]}.")
-            
-
-def lowest():
-    high = 0
-    max = int(studentMarks[0])
-    for i in range(len(studentMarks)):
-        cur_val = int(studentMarks[i])            
-        if cur_val < max:           # copy and pasted same thing just change the symbol, > to <
-            max = cur_val 
-            high = i
-    print(f"Hightest Marks is : {studentMarks[high]} by {studentNames[high]}.")
-
-def average():
-    sum = 0
-    count = 0
-    for i in range(len(studentMarks)):
-        sum += int(studentMarks[i])
-        count = int(studentMarks[-1])
-
-    print(f"The Average of Students are : {sum / count}")
-
-
-def close():
-    raise SystemExit
 
 # Main  function
 while True:
@@ -95,14 +56,15 @@ while True:
         case '1' | 'Add Student' | 'Add':
             addStudentAndMarks()
         case '2' | 'Display Students' | 'Display':
-            display()
+            print(studentNames, studentMarks)
         case '3' | 'Highest Marks' | 'Highest':
-            highest_Marks()
+            print("The highest marks = ",max(studentMarks))
         case '4' | 'Lowest Marks' | 'Lowest':
-            lowest()
+            print("The lowest Marks = ",min(studentMarks))
         case '5' | 'Average' | 'Average Marks':
-            average()
+            average = (sum(studentMarks)//len(studentMarks))
+            print(average)
         case '6' | 'Exit':
-            close()
+            break
         case _:
             print("invalid@@@@@@@@@@@@@@!!!!!!!!!!!!!!!!!!!!!!!!!!!@@@@@@@@@")
